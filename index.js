055f703baabe47ad55f80b6d3565739d340df508
azmi adan <azmi.adan2032@gmail.com>
2024-07-11 22:15:19 +0300

    add files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ];
+  
+  function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+  }
+  
+  const winYear = superbowlWin(record);
+  console.log(winYear); // Output: "2015" (or undefined if no win is found)
